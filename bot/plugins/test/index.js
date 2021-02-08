16fe56b4f952b48ee9ae70f311cf793131897a77
xianbei233 <455482921@qq.com>
2021-02-08 09:13:53 +0800

    ✨feat: 进度暂存

--- bot/plugins/test/index.js
+++ bot/plugins/test/index.js
@@ -3,9 +3,16 @@ const { default: Bot } = require("el-bot");
  *
  * @param {Bot} ctx
  */
-module.exports = (ctx) => {
-  const mirai = ctx.mirai;
+module.exports = (ctx, options) => {
+  const {
+    mirai,
+    logger,
+    el: { config },
+  } = ctx;
+
   mirai.on("message", (msg) => {
+    console.log("msg", msg, options, config);
+    logger.success("人类的伟大就是勇气的伟大");
     msg.reply(
       "我是一个无情的自定义测试插件，可以到 el/index.yml plugins.custom 处关闭我。"
     );
---
+++ bot/plugins/test/options.js
@@ -0,0 +1,8 @@
+const test = [
+  {
+    henshin: true,
+  },
+];
+
+export { test as default }; // ES标准拿不到
+// module.exports = test; // CommonJS拿得到
